a93aa8c2d4f8139a7f394ed24d2d8cf33b76afee
chrisevans--laptop <evansc@mssm.org>
2017-05-15 22:18:53 -0400

    added sign in and out, time as well

--- public/index.js
+++ public/index.js
@@ -1,47 +1,48 @@
+var students;
 $(document).ready(function(){
-  // $.getJSON("/listofnames", {}, function(dat, stat){
-  //   console.log(dat);
-  var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var dat = {
-    chris: {
-      name: "Chris Evans",
-      signedIn: true
-    },
-    jackson: {
-      name: "Jackson Perkins",
-      signedIn: false
-    },
-    noah:{
-      name: "Noah Burck",
-      signedIn: false
-    }
-  };
+  $.getJSON("/listofnames", {}, function(dat, stat){
+    students = dat;
+    console.log(students);
+    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-  $("#buttonArray").append("<tr>");
-  for (var i in alphabet){
-        $("#buttonArray").append("<td><a href=\"#name_" + alphabet[i] + "\">"+ alphabet[i] + "</a></td>");
-        if (i % 7 == 6) {
-          $("#buttonArray").append("</tr><tr>");
+    $("#buttonArray").append("<tr>");
+    for (var i in alphabet){
+          $("#buttonArray").append("<td><a href=\"#name_" + alphabet[i] + "\">"+ alphabet[i] + "</a></td>");
+          if (i % 7 == 6) {
+            $("#buttonArray").append("</tr><tr>");
+        }
+    }
+
+    for(var i in alphabet){
+      if (alphabet.hasOwnProperty(i)) {
+        $("#names").append("<tr><table><tr><th id=\"name_" + alphabet[i] + "\">" + alphabet[i].toUpperCase() + "</th></tr></table></tr>");
       }
-  }
+    }
+    for (var i in dat){
+      $("#name_" + dat[i].name.substr(0,1).toLowerCase()).append("<tr><td>" + dat[i].name + "</td><td>" + function(){
+        if(dat[i].signedIn){
+          return "Signed in</td><td id=\"" + i + "\" class=\"sign\"><button onclick=\'signOut(\"" + dat[i].id + "\")\'>Sign Out</button>";
+        }else{
+          $("#signedOutTable").append("<tr><td>" +
+          dat[i].name + "</td><td>" + dat[i].timeOut + "</td><td id=\"" + i + "\" class=\"sign\"><button onclick=\'signIn(\"" + dat[i].id + "\")\'>Sign In</button></td></tr>");
+          return "Signed out</td><td id=\"" + i + "\" class=\"sign\"><button onclick=\'signIn(\"" + dat[i].id + "\")\'>Sign In</button>";}
+        }() + "</td></tr>");
+    }
 
-  for(var i in alphabet){
-    if (alphabet.hasOwnProperty(i)) {
-      $("#names").append("<tr><table><tr><th id=\"name_" + alphabet[i] + "\">" + alphabet[i].toUpperCase() + "</th></tr></table></tr>");
-    }
-  }
-  for (var i in dat){
-    $("#name_" + dat[i].name.substr(0,1).toLowerCase()).append("<tr><td>" + dat[i].name + "</td><td>" + function(){if(dat[i].signedIn){return "Signed in</td><td id=\"" + i + "\" class=\"sign\">Sign out";} else{$("#signedOutTable").append("<tr><td>" +
-    dat[i].name + "</td><td id=\"" + i + "\" class=\"sign\">Sign in</td></tr>");return "Signed out</td><td id=\"" + i + "\" class=\"sign\">Sign in";}}() + "</td></tr>");
-  }
 
-  $(".sign").click(function(){
-    dat[$(this).attr('id')].signedIn=!dat[$(this).attr('id')].signedIn;
-    if(!dat[$(this).attr('id')].signedIn){
-      //go to sign out page
-    }
-    else{
-      window.location.reload();
-    }
-  })
+  });
 });
+
+function signIn(student){
+  console.log("signIn called! " + student);
+  console.log('HERE IS THE LOCAL OBJECT '+ students);
+  console.log(students);
+  console.log(students[student]);
+  console.log(students[student].name);
+  $.post("/signIn", {student: students[student].id});
+  window.location.reload();
+}
+function signOut(student){
+  $.post("/signOut", {student: students[student].id});
+  window.location.reload();
+}
--- server.js
+++ server.js
@@ -1,11 +1,87 @@
-
+var url = require('url-parse');
+var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-var students = {};
+
+var students = {
+  evansc: {
+    name: "Chris Evans",
+    signedIn: true,
+    id:"evansc",
+    timeOut: 0
+  },
+  perkinsj: {
+    name: "Jackson Perkins",
+    signedIn: false,
+    id:"perkinsj",
+    timeOut: 0
+  },
+  burckn:{
+    name: "Noah Burck",
+    signedIn: false,
+    id:"burckn",
+    timeOut: 0
+
+  }
+};
 
 app.get("/listofnames", function(req, res){
-  res.send();
+  res.send(students);
 });
+
+
+app.post("/signIn", function(req,res){
+  console.log("signOut POST Called!");
+  console.log(req.body);
+  console.log(req.body.student);
+  students[req.body.student].signedIn = true;
+  console.log(students);
+
+});
+
+app.post("/signOut", function(req,res){
+  console.log("signOut POST Called!");
+  students[req.body.student].timeOut = getTime();
+  students[req.body.student].signedIn = false;
+
+});
+
+function getTime() {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+
+    return hour + ":" + min;
+
+}
+
+function getDate(){
+
+  var date = new Date();
+
+  var year = date.getFullYear();
+
+  var month = date.getMonth() + 1;
+  month = (month < 10 ? "0" : "") + month;
+
+  var day  = date.getDate();
+  day = (day < 10 ? "0" : "") + day;
+
+  return year + ":" + month + ":" + day;
+
+}
 
 
 app.use(express.static('public'));
