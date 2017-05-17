67ce1a564cd29d52036a183104ce4b9e2f7d67bd
Noah Burck <Noah Burck>
2017-05-17 18:21:24 -0400

    brought over chris' server stuff, added json save support, fixed a few bugs, added SAFE custom destinations

--- public/index.js
+++ public/index.js
@@ -1,47 +1,64 @@
+var students;
 $(document).ready(function(){
-  // $.getJSON("/listofnames", {}, function(dat, stat){
-  //   console.log(dat);
-  var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var dat = {
-    chris: {
-      name: "Chris Evans",
-      signedIn: true
-    },
-    jackson: {
-      name: "Jackson Perkins",
-      signedIn: false
-    },
-    noah:{
-      name: "Noah Burck",
-      signedIn: false
-    }
-  };
+  $.getJSON("/listofnames", {}, function(dat, stat){
+    students = dat;
+    console.log(students);
+    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-  $("#buttonArray").append("<tr>");
-  for (var i in alphabet){
-        $("#buttonArray").append("<td><a href=\"#name_" + alphabet[i] + "\">"+ alphabet[i] + "</a></td>");
-        if (i % 7 == 6) {
-          $("#buttonArray").append("</tr><tr>");
+    $("#buttonArray").append("<tr>");
+    for (var i in alphabet){
+          $("#buttonArray").append("<td><a href=\"#name_" + alphabet[i] + "\">"+ alphabet[i].toUpperCase() + "</a></td>");
+          if (i % 7 == 6) {
+            $("#buttonArray").append("</tr><tr>");
+        }
+    }
+
+    for(var i in alphabet){
+      if (alphabet.hasOwnProperty(i)) {
+        $("#names").append("<tr><table><tr><th id=\"name_" + alphabet[i] + "\">" + alphabet[i].toUpperCase() + "</th></tr></table></tr>");
       }
-  }
-
-  for(var i in alphabet){
-    if (alphabet.hasOwnProperty(i)) {
-      $("#names").append("<tr><table><tr><th id=\"name_" + alphabet[i] + "\">" + alphabet[i].toUpperCase() + "</th></tr></table></tr>");
     }
-  }
-  for (var i in dat){
-    $("#name_" + dat[i].name.substr(0,1).toLowerCase()).append("<tr><td>" + dat[i].name + "</td><td>" + function(){if(dat[i].signedIn){return "Signed in</td><td id=\"" + i + "\" class=\"sign\">Sign out";} else{$("#signedOutTable").append("<tr><td>" +
-    dat[i].name + "</td><td id=\"" + i + "\" class=\"sign\">Sign in</td></tr>");return "Signed out</td><td id=\"" + i + "\" class=\"sign\">Sign in";}}() + "</td></tr>");
-  }
-
-  $(".sign").click(function(){
-    dat[$(this).attr('id')].signedIn=!dat[$(this).attr('id')].signedIn;
-    if(!dat[$(this).attr('id')].signedIn){
-      //go to sign out page
+    for (var i in dat){
+      $("#name_" + dat[i].name.substr(0,1).toLowerCase()).append("<tr><td>" + dat[i].name + "</td><td>" + function(){
+        if(dat[i].signedIn){
+          return "Signed in</td><td id=\"" + i + "\" class=\"sign\"><button class=\"signout-button\" onclick=\'getCookie(\"" + dat[i].id + "\")\'>Sign Out</button>";
+        }else{
+          if(!$("#titleRow").length){
+            $("#signedOutTable").append("<tr id = \"signedOutTable\"><th>Name</th><th>Time</th><th>Date</th><th>Destination</th><th>Sign</th></tr>");
+          }
+          $("#signedOutTable").append("<tr><td>" +
+          dat[i].name + "</td><td>" + dat[i].timeOut + "</td><td>" + dat[i].dateOut + "</td><td>" + dat[i].dest + "</td><td id=\"" + i + "\" class=\"sign\"><button class=\"signin-button\" onclick=\'signIn(\"" + dat[i].id + "\")\'>Sign In</button></td></tr>");
+          return "Signed out</td><td id=\"" + i + "\" class=\"sign\"><button class=\"signin-button\" onclick=\'signIn(\"" + dat[i].id + "\")\'>Sign In</button>";}
+        }() + "</td></tr>");
     }
-    else{
+
+
+  });
+});
+
+function signIn(student){
+  console.log("signIn called! " + student);
+  console.log('HERE IS THE LOCAL OBJECT '+ students);
+  console.log(students);
+  console.log(students[student]);
+  console.log(students[student].name);
+  $.post("/signIn", {student: students[student].id});
+  window.location.reload();
+}
+function getCookie(student){
+  $.post("/getCookie", {student: students[student].id});
+  window.location.replace("/destinations.html");
+  //window.location.reload();
+}
+
+window.setInterval(function(){
+  $.getJSON("/listofnames", function(dat, stat){
+    if(dat != students){
       window.location.reload();
     }
-  })
-});
+  });
+}, 60000);
+
+function refresh(){
+
+}
--- server.js
+++ server.js
@@ -1,17 +1,154 @@
-
+var url = require('url-parse');
+var express = require('express');
 var app = express();
+var url = require('url-parse');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var fs = require('fs');
+var sanitizer = require("sanitizer");
+
+
+var obj = JSON.parse(fs.readFileSync('studentdir.json', 'utf8'));
+
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.use(cookieParser());
+
+/*app.use(function (req, res, next) {
+// check if client sent cookie
+var cookie = req.cookies.currentStu;
+if (cookie === undefined)
+{
+// no: set a new cookie
+var randomNumber=Math.random().toString();
+randomNumber=randomNumber.substring(2,randomNumber.length);
+res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
+console.log('cookie created successfully');
+}
+else
+{
+// yes, cookie was already present
+console.log('cookie exists', cookie);
+}
+next(); // <-- important!
+});*/
 
 var students = {};
 
-app.get("/listofnames", function(req, res){
-  res.send();
+function makestudents(_name, _last, _first,  _signedIn, _id, _timeOut, _dateOut, _dest){
+  var id = _id;
+  var anObj={
+    name: _name,
+    first: _first,
+    last: _last,
+    signedIn: _signedIn,
+    id: _id,
+    timeOut: _timeOut,
+    dateOut: _dateOut,
+    dest: _dest
+  }
+  students[id] = anObj;
+}
+
+for (var names in obj) {
+  if (obj.hasOwnProperty(names)) {
+    //console.log(names + " -> " + obj[names].last);
+    var s = obj[names];
+    makestudents(s.name, s.last, s.first, s.signedIn, s.id, s.timeOut, s.dateOut, s.dest);
+  }
+}
+
+console.log(students);
+
+
+app.get('/',function(req,res){
+
+  res.redirect("/index.html");
+
 });
+
+
+app.get("/listofnames", function(req, res){
+  res.send(students);
+});
+
+
+app.post("/signIn", function(req,res){
+  console.log("signIn POST Called!");
+  console.log("sign in "+ req.body.student);
+  students[req.body.student].signedIn = true;
+  students[req.body.student].dest = "null";
+  students[req.body.student].timeOut = 0;
+  students[req.body.student].dateOut = 0;
+  console.log(students[req.body.student]);
+  fs.writeFileSync('studentdir.json', JSON.stringify(students, null, 4));
+  fs.appendFileSync("logfile.log", "signIn POST Called on student " + students[req.body.student].name + "\nDate/Time: " + getDate() +"/" + getTime() + "\n\n");
+});
+
+app.post("/getCookie", function(req,res){
+  res.cookie("currentStu", req.body.student).send('Cookie is set');
+  res.end('done');
+
+});
+
+app.post("/destinations.html",function(req, res){ //signOut
+  console.log("signOut POST Called!");
+  console.log("Cookie: "+ req.cookies.currentStu);
+  console.log("current destination as of /destinations "+ sanitizer.sanitize(req.body.dest));
+  console.log("students "+ students);
+  students[req.cookies.currentStu].dest = sanitizer.sanitize(req.body.dest);
+  students[req.cookies.currentStu].timeOut = getTime();
+  students[req.cookies.currentStu].dateOut = getDate();
+  students[req.cookies.currentStu].signedIn = false;
+  fs.writeFileSync('studentdir.json', JSON.stringify(students, null, 4));
+  fs.appendFileSync("logfile.log", "signOut POST Called on student " + students[req.cookies.currentStu].name + "\nLocation: " + students[req.cookies.currentStu].dest + "\nDate/Time: " + students[req.cookies.currentStu].dateOut + "/" + students[req.cookies.currentStu].timeOut + "\n\n");
+  res.redirect("/");
+});
+
+function getTime() {
+
+  var date = new Date();
+
+  var hour = date.getHours();
+  hour = (hour < 10 ? "0" : "") + hour;
+
+  var min  = date.getMinutes();
+  min = (min < 10 ? "0" : "") + min;
+
+  var sec  = date.getSeconds();
+  sec = (sec < 10 ? "0" : "") + sec;
+
+
+  return hour + ":" + min;
+
+}
+
+function getDate(){
+
+  var date = new Date();
+
+  var year = date.getFullYear();
+
+  var month = date.getMonth() + 1;
+  month = (month < 10 ? "0" : "") + month;
+
+  var day  = date.getDate();
+  day = (day < 10 ? "0" : "") + day;
+
+  return month + "/" + day + "/" + year;
+
+}
 
 
 app.use(express.static('public'));
 
 var server = app.listen(8081, function() {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("Example app listening at http://%s:%s", host, port);
+  var port = server.address().port;
+  require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+    console.log('Example app listening at http://%s:%s', add, port);
+  })
+  fs.appendFileSync("logfile.log", "Started server on port " + port + " at " + getTime() + " on " + getDate() + "\n\n\n");
 });
